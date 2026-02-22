998ab91151bfc92feb94ca534d5e00448edd4113
Jeeva M <jeevam182005@gmail.com>
2026-02-22 18:58:45 +0530

    flask structing progress

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, request, render_template
 
 
 app=Flask(__name__)
-@app.route("/",methods=["GET", "POST"])
 
+@app.route("/",methods=["GET", "POST"])
 def homepage():
     return render_template("indexn.html")
+
+@app.route("/faculty",methods=["GET", "POST"])
+def faculty():
+    return render_template("faculty.html")
 
 
 if __name__=="__main__":
